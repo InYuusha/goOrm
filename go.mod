17ead60689a175e8737e61490c198ce442017e58
InYuusha <asuse975@gmail.com>
2021-08-01 01:45:16 +0530

    create , getAll, delete

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module api
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
--- user.go
+++ user.go
@@ -1,9 +1,11 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -11,35 +13,65 @@ import(
 var db *gorm.DB
 var err error
 
-type User struct{
+type User struct {
 	gorm.Model
-	Name string
+	Name  string
 	Email string
 }
-func InitialMigration(){
-	db,err = gorm.Open("sqlite3","test.db")
+
+func InitialMigration() {
+	db, err = gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("Failed to connect")
 	}
-	defer db.close() //if panicked ,
+	defer db.Close() //if panicked ,
 	// do this before exit(LIFO order for multiple defer)
-
 	db.AutoMigrate(&User{}) //migrate schema ,create tables update table ,(wont del)
 }
+func AllUsers(res http.ResponseWriter, req *http.Request) {
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Couldnt connect to the database")
+	}
+	defer db.Close()
 
-func AllUsers(res http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(res,"All users Endpoint ")
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(res).Encode(users)
 }
 
-func NewUser(res http.ResponseWriter,req*http.Request){
-	fmt.Fprintf(res,"New User Endopint")
+func NewUser(res http.ResponseWriter, req *http.Request) {
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Couldnt connect to database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(req)
+	name := vars["name"]
+	email := vars["email"]
+
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(res, "New User Created")
 }
 
-func DeleteUser(res http.ResponseWriter,req*http.Request){
-	fmt.Fprintf(res,"Delete user Endpoint")
+func DeleteUser(res http.ResponseWriter, req *http.Request) {
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Couldnt connect to database")
+	}
+	defer db.Close()
+	vars := mux.Vars(req)
+	name := vars["name"]
+
+	var user User
+	db.Where("name =?", name).Find(&user) //populates the result in user obj
+	db.Delete(&user)
+
+	fmt.Fprintf(res, "User Successfully Deleted")
 }
 
-func UpdateUser(res http.ResponseWriter,req*http.Request){
-	fmt.Fprintf(res,"Update user Endpoint")
+func UpdateUser(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Update user Endpoint")
 }
